82b01d0e7ae96d268cb3fbd37300b4e3c0a43f3a
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-04-29 17:04:24 +0100

    identified that it's a fixmap

--- msgpack.cpp
+++ msgpack.cpp
@@ -45,67 +45,166 @@ typedef enum {
   failure,
 } type;
 
+const char *type_name(type ty) {
+  switch (ty) {
+  case posfixint:
+    return "posfixint";
+  case fixmap:
+    return "fixmap";
+  case fixarray:
+    return "fixarray";
+  case fixstr:
+    return "fixstr";
+  case nil:
+    return "nil";
+  case f:
+    return "f";
+  case t:
+    return "t";
+  case bin8:
+    return "bin8";
+  case bin16:
+    return "bin16";
+  case bin32:
+    return "bin32";
+  case ext8:
+    return "ext8";
+  case ext16:
+    return "ext16";
+  case ext32:
+    return "ext32";
+  case float32:
+    return "float32";
+  case float64:
+    return "float64";
+  case uint8:
+    return "uint8";
+  case uint16:
+    return "uint16";
+  case uint32:
+    return "uint32";
+  case uint64:
+    return "uint64";
+  case int8:
+    return "int8";
+  case int16:
+    return "int16";
+  case int32:
+    return "int32";
+  case int64:
+    return "int64";
+  case fixext1:
+    return "fixext1";
+  case fixext2:
+    return "fixext2";
+  case fixext4:
+    return "fixext4";
+  case fixext8:
+    return "fixext8";
+  case fixext16:
+    return "fixext16";
+  case str8:
+    return "str8";
+  case str16:
+    return "str16";
+  case str32:
+    return "str32";
+  case array16:
+    return "array16";
+  case array32:
+    return "array32";
+  case map16:
+    return "map16";
+  case map32:
+    return "map32";
+  case negfixint:
+    return "negfixint";
+  case failure:
+    return "failure";
+  }
 }
+} // namespace msgpack
 
 msgpack::type parse_type(unsigned char x) {
   using namespace msgpack;
-  switch(x)
-    {
-    case  0x00 ... 0x7f:     
-      return posfixint;
-    case 0x80 ... 0x8f:
-      return fixmap;
-    case 0x90 ... 0x9f:
-      return fixarray;
-    case 0xa0 ... 0xbf:
-      return fixstr;
-    case 0xc0: return nil;
-    case 0xc1: return failure;
-    case 0xc2: return f;
-    case 0xc3: return t;
-    case 0xc4: return bin8;
-    case 0xc5: return bin16;
-    case 0xc6: return bin32;
-    case 0xc7: return ext8;
-    case 0xc8: return ext16;
-    case 0xc9: return ext32;
-    case 0xca: return float32;
-    case 0xcb: return float64;
-    case 0xcc: return uint8;
-    case 0xcd: return uint16;
-    case 0xce: return uint32;
-    case 0xcf: return uint64;
-    case 0xd0: return int8;
-    case 0xd1: return int16;
-    case 0xd2: return int32;
-    case 0xd3: return int64;
-    case 0xd4: return fixext1;
-    case 0xd5: return fixext2;
-    case 0xd6: return fixext4;
-    case 0xd7: return fixext8;
-    case 0xd8: return fixext16;
-    case 0xd9: return str8;
-    case 0xda: return str16;
-    case 0xdb: return str32;
-    case 0xdc: return array16;
-    case 0xdd: return array32;
-    case 0xde: return map16;
-    case 0xdf: return map32;
-    case 0xe0 ... 0xff: return negfixint;
-      
-      
-
-        
-        
-      
-      
-
-    default:
-      return failure;
-    }
-
-
-
+  switch (x) {
+  case 0x00 ... 0x7f:
+    return posfixint;
+  case 0x80 ... 0x8f:
+    return fixmap;
+  case 0x90 ... 0x9f:
+    return fixarray;
+  case 0xa0 ... 0xbf:
+    return fixstr;
+  case 0xc0:
+    return nil;
+  case 0xc1:
+    return failure;
+  case 0xc2:
+    return f;
+  case 0xc3:
+    return t;
+  case 0xc4:
+    return bin8;
+  case 0xc5:
+    return bin16;
+  case 0xc6:
+    return bin32;
+  case 0xc7:
+    return ext8;
+  case 0xc8:
+    return ext16;
+  case 0xc9:
+    return ext32;
+  case 0xca:
+    return float32;
+  case 0xcb:
+    return float64;
+  case 0xcc:
+    return uint8;
+  case 0xcd:
+    return uint16;
+  case 0xce:
+    return uint32;
+  case 0xcf:
+    return uint64;
+  case 0xd0:
+    return int8;
+  case 0xd1:
+    return int16;
+  case 0xd2:
+    return int32;
+  case 0xd3:
+    return int64;
+  case 0xd4:
+    return fixext1;
+  case 0xd5:
+    return fixext2;
+  case 0xd6:
+    return fixext4;
+  case 0xd7:
+    return fixext8;
+  case 0xd8:
+    return fixext16;
+  case 0xd9:
+    return str8;
+  case 0xda:
+    return str16;
+  case 0xdb:
+    return str32;
+  case 0xdc:
+    return array16;
+  case 0xdd:
+    return array32;
+  case 0xde:
+    return map16;
+  case 0xdf:
+    return map32;
+  case 0xe0 ... 0xff:
+    return negfixint;
+  default:
+    return failure;
+  }
 }
 
 TEST_CASE("str") { CHECK(helloworld_msgpack_len != 0); }
@@ -113,6 +212,6 @@ TEST_CASE("str") { CHECK(helloworld_msgpack_len != 0); }
 TEST_CASE("hello world") {
   SECTION("sanity checks") {
     unsigned char start = helloworld_msgpack[0];
-    CHECK(parse_type(start) == msgpack::map32);
+    CHECK(parse_type(start) == msgpack::fixmap);
   }
 }
