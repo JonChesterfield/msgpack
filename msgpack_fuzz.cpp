3999583dab08cef765f7e3809e046a7190fa6039
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-05-01 23:05:59 +0100

    fuzz truncation

--- msgpack_fuzz.cpp
+++ msgpack_fuzz.cpp
@@ -37,11 +37,13 @@ TEST_CASE("check all short byte sequences") {
   SECTION("unary") {
     functors f = readall_functors();
     unsigned char *byte = (unsigned char *)malloc(1);
+    bool ok = true;
     for (unsigned i = 0; i < 256; i++) {
       byte[0] = (unsigned char)i;
       const unsigned char *res = handle_msgpack({byte, byte + 1}, f);
-      CHECK((res == 0 || res == byte + 1));
+      ok &= (res == 0 || res == byte + 1);
     }
+    CHECK(ok);
     free(byte);
   }
 
@@ -49,14 +51,16 @@ TEST_CASE("check all short byte sequences") {
     functors f = readall_functors();
     unsigned N = 2;
     unsigned char *byte = (unsigned char *)malloc(N);
+    bool ok = true;
     for (unsigned i = 0; i < 256; i++) {
       for (unsigned j = 0; j < 256; j++) {
         byte[0] = (unsigned char)i;
         byte[1] = (unsigned char)j;
         const unsigned char *res = handle_msgpack({byte, byte + N}, f);
-        CHECK((res == 0 || res == byte + 1 || res == byte + 2));
+        ok &= (res == 0 || res == byte + 1 || res == byte + 2);
       }
     }
+    CHECK(ok);
     free(byte);
   }
 
@@ -64,6 +68,7 @@ TEST_CASE("check all short byte sequences") {
     functors f = readall_functors();
     unsigned N = 3;
     unsigned char *byte = (unsigned char *)malloc(N);
+    bool ok = true;
     for (unsigned i = 0; i < 256; i++) {
       continue; // skip this for now
       for (unsigned j = 0; j < 256; j++) {
@@ -72,11 +77,12 @@ TEST_CASE("check all short byte sequences") {
           byte[1] = (unsigned char)j;
           byte[2] = (unsigned char)k;
           const unsigned char *res = handle_msgpack({byte, byte + N}, f);
-          CHECK((res == 0 || res == byte + 1 || res == byte + 2 ||
-                 res == byte + 3));
+          ok &= (res == 0 || res == byte + 1 || res == byte + 2 ||
+                 res == byte + 3);
         }
       }
     }
+    CHECK(ok);
     free(byte);
   }
 }
@@ -86,21 +92,40 @@ TEST_CASE("from urandom") {
   REQUIRE(fd >= 0);
 
   unsigned reps = 100;
-  unsigned N = 1024 * 40;
+  unsigned N = 1024;
+  unsigned offset = 50;
   unsigned char *bytes = (unsigned char *)malloc(N);
   functors f = readall_functors();
 
   bool ok = true;
   for (unsigned r = 0; r < reps; r++) {
     ssize_t result = read(fd, bytes, N);
-    REQUIRE(result >= 0);
+    ok &= result >= 0;
+    if (result < 0) {
+      break;
+    }
 
-    for (unsigned s = 0; s < 50; s++) {
+    for (unsigned s = 0; s < offset; s++) {
+      if (s > N) {
+        continue;
+      }
       for (unsigned i = 0; i < 256; i++) {
         bytes[s] = (unsigned char)i;
         const unsigned char *res =
             handle_msgpack({bytes + s, bytes + N - s}, f);
         bool in_bounds = (res >= (bytes + s)) && (res <= (bytes + N - s));
+        ok &= ((res == 0) || in_bounds);
+      }
+    }
+
+    for (unsigned w = 0; w < offset; w++) {
+      if (w > N) {
+        continue;
+      }
+      for (unsigned i = 0; i < 256; i++) {
+        bytes[0] = (unsigned char)i;
+        const unsigned char *res = handle_msgpack({bytes, bytes + w}, f);
+        bool in_bounds = (res >= bytes) && (res <= (bytes + w));
         ok &= ((res == 0) || in_bounds);
       }
     }
